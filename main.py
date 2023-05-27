ed602689a4b7136000dc224f505c0edb4a9eb3db
Balasubramanyam Evani <bevani@Balasubramanyams-MacBook-Air.local>
2023-05-27 13:20:37 -0400

    Gradient wasn't being logged for learnable pos embedding. Fixed that. Also added inline comments

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ Author: Balasubramanyam Evani
 Date: 2023-05-27
 
 Description: Main Script
+Does two experiments:
+
+Experiment for Hypothesis 1: 
+As we increase the number of multi-head self-attention layers, the overall computing 
+should be computationally expensive. Since, the self-attention operation has a quadratic 
+time complexity concerning the sequence length. I expect to observe prohibitively slower 
+performance or requirement of significant computational resources for longer sequences.
+
+Experiment for Hypothesis 2: Only stacking Multi-head attention layers might result in a 
+vanishing gradient problem. The original transformer paper used MHA in conjecture with 
+Residual connections. Hence, I hope to see average gradient value per layer become smaller 
+and smaller as gradients flow toward the initial layers.
 """
 import torch
 import wandb
@@ -19,8 +31,13 @@ if __name__ == "__main__":
 
     print(opts)
 
+    # Initialize Weights & Biases (wandb) with the specified project name
     wandb.init(project="script-chain-health-exps")
+
+    # Update the wandb configuration with the options obtained
     wandb.config.update(opts)
+
+    # Set the run name for wandb to the specified run_name from the options
     wandb.run.name = opts.run_name
 
     pe_learnable = opts.pe_learnable
@@ -29,13 +46,19 @@ if __name__ == "__main__":
     num_self_attn_layers = opts.fixed_self_attn_layers
     num_texts = opts.num_texts
 
+    # Get the device to be used for training
     device = get_device()
+
+    # Create a dummy dictionary
     dictionary = dummy_dictionary()
+
+    # Obtain the vocabulary size from the dummy dictionary
     vocab_size = len(dictionary)
 
     # Exp 1: Different Seq Lengths
-
+    # Iterate over each sequence length in seq_lens
     for seq_len in seq_lens:
+        # Create a dummy dataset with the specified number of texts and sequence length
         dataset = DummyDataset(num_texts=num_texts, seq_len=seq_len, dictionary=dictionary)
         dataloader = torch.utils.data.DataLoader(
             dataset,
@@ -43,6 +66,8 @@ if __name__ == "__main__":
             pin_memory=True,
             shuffle=True,
         )
+        # Create a dummy classifier model with the specified number of self-attention layers, 
+        # sequence length, vocab size, and pe_learnable flag
         model = DummyClassifier(
             enc_self_attn_layers=num_self_attn_layers,
             seq_len=seq_len,
@@ -57,13 +82,17 @@ if __name__ == "__main__":
             momentum=0.9,
             weight_decay=0,
         )
+        # Run the model for one epoch and collect the execution time
         execution_time, _ = run(model, criterion, optimizer, dataloader, device, epochs=1)
+        
+        # Log the experiment results to wandb for Exp 1
         wandb.log({"Exp1: Input Sequence length": seq_len, "Exp1: Execution time (s)": execution_time})
 
     fixed_seq_len = opts.fixed_seq_len
     self_attn_layers = opts.self_attn_layers
 
     # Exp 2: Different Self Attention layers
+    # Create a dataset with the fixed sequence length
     dataset = DummyDataset(num_texts=num_texts, seq_len=fixed_seq_len, dictionary=dictionary)
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -71,7 +100,11 @@ if __name__ == "__main__":
         pin_memory=True,
         shuffle=True,
     )
+
+    # Iterate over each number of self-attention layers in self_attn_layers
     for num_layers in self_attn_layers:
+        # Create a dummy classifier model with the specified number of self-attention layers, 
+        # fixed sequence length, vocab size, and pe_learnable flag
         model = DummyClassifier(
             enc_self_attn_layers=num_layers,
             seq_len=fixed_seq_len,
@@ -86,6 +119,8 @@ if __name__ == "__main__":
             momentum=0.9,
             weight_decay=0,
         )
+        # Run the model training for one epoch, 
+        # collect the execution time, and retrieve the gradient accumulator
         execution_time, grad_accumulator = run(model, criterion, optimizer, dataloader, device, epochs=1, log_grad=True)
         
         log_info = {
@@ -93,10 +128,12 @@ if __name__ == "__main__":
             "Exp2: Execution time (s)": execution_time
         }
 
+        # Log the gradients for specific keys (lower layer and learnable embedding)
         for key in grad_accumulator.keys():
-            if "embedding" in key or "enc_modules.0" in key:
+            if "embedding" in key or "enc_modules.0" in key or "encoder.pe.pe.weight" in key:
                 log_info[f"Exp2: [Gradient] {key}"] = grad_accumulator[key]
 
+        # Log the experiment results to wandb for Exp 2
         wandb.log(log_info)
 
     wandb.run.finish()
--- opts.py
+++ opts.py
@@ -2,66 +2,91 @@
 Author: Balasubramanyam Evani
 Date: 2023-05-27
 
-Description: Main scripts options
+Description: Main script options
 """
 import argparse
 
 
 def get_opts():
+    # Create an ArgumentParser object for cmd line argument parsing
     parser = argparse.ArgumentParser(
         description="ScriptChain Health Assessment Experiments"
     )
+
+    # --num_texts argument: specifies the number of texts for generating a dummy dataset
     parser.add_argument(
         "--num_texts",
         type=int,
         default=1000,
         help="for generating dummy dataset"
     )
+
+    # --pe_learnable argument: specifies whether to use learnable positional embedding or not
     parser.add_argument(
         "--pe_learnable",
         default=False,
         action="store_true",
         help="whether to use learnable positional embedding"
     )
+
+    # --run_name argument: specifies the run name tag for wandb
     parser.add_argument(
         "--run_name",
         type=str,
         default="Run using non learnable positional embedding",
         help="run name tag for wandb",
     )
+
+    # --batch_size argument: specifies the batch size used to run one epoch
     parser.add_argument(
         "--batch_size",
         type=int,
         default=32,
         help="Batch Size",
     )
+
+    # --seq_lens argument: specifies the list of sequence lengths used 
+    # in the first part of the experiment
     parser.add_argument(
         "--seq_lens",
         nargs="+",
         default=[2**i for i in range(6, 10)],
         help="used in first part of experiment",
     )
+
+    # --fixed_self_attn_layers argument: specifies the number of self-attention layers 
+    # for stacking, used in the first part of the experiment
     parser.add_argument(
         "--fixed_self_attn_layers",
         type=int,
         default=8,
         help="number of self attention layers for stacking, used in first part of experiment",
     )
+
+    # --fixed_seq_len argument: specifies the sequence length used to 
+    # run the second part of the experiment
     parser.add_argument(
         "--fixed_seq_len",
         type=int,
         default=64,
         help="used to run second part of experiment",
     )
+
+    # --self_attn_layers argument: specifies the list of numbers of self-attention 
+    # layers used in the second part of the experiment
     parser.add_argument(
         "--self_attn_layers",
         nargs="+",
         default=[2**i for i in range(1, 8)],
         help="used in second part of experiment",
     )
+
+    # Parse the command-line arguments and store the values in the `opts` variable
     opts = parser.parse_args()
 
+    # Convert the sequence lengths and numbers of self-attention layers to integers
     opts.seq_lens = [int(item) for item in opts.seq_lens]
     opts.self_attn_layers = [int(item) for item in opts.self_attn_layers]
     
+    # Return the obtained options
     return opts
